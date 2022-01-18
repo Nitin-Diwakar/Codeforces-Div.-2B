79c8c7a0a79a643d690e07f92c09a7f65cfba8f7
Nitin Diwakar <diwakarnitin509@gmail.com>
2022-01-18 12:16:06 +0530

    Keyboard layouts

---
+++ Keyboard Layouts.py
@@ -0,0 +1,13 @@
+a = list(input())
+b  = list(input())
+isomorphic = dict(zip(a,b))
+string = list(input())
+result = []
+for i in string:
+    if i.lower() in isomorphic and i.islower():
+        result.append(isomorphic[i.lower()])
+    elif i.lower() in isomorphic and i.isupper():
+        result.append((isomorphic[i.lower()]).upper())
+    else:
+        result.append(i)
+print("".join(result))
