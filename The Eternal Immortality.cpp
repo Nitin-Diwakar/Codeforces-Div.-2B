514e17d55cfb051fbe9d7e21725042b92541de78
Nitin Diwakar <diwakarnitin509@gmail.com>
2022-02-19 09:19:43 +0530

    19-02-2022

--- The Eternal Immortality.cpp
+++ The Eternal Immortality.cpp
@@ -1,25 +1,17 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
+long long L,R;
+int ans;
 int main()
 {
-    long long a,b;
-    long long ans;//Take care, integer overflow can emerge everywhere!
-    while(~scanf("%lld%lld",&a,&b))
-    {
-        ans=1;
-        for(long long i=a+1;i<=b;i++)
-        {
-            ans=(ans*(i%10))%10;
-            if(!ans) break;
-        }
-        printf("%lld\n",ans);
-    }
-    return 0;
+	scanf("%lld%lld",&L,&R);
+	if (R-L>=10) printf("%d\n",0);
+	else
+	{
+		ans=1;
+		for (long long i=L+1;i<=R;i++)
+			ans=(1LL*ans*(i%10))%10;
+		printf("%d\n",ans);
+	}
+	return 0;
 }
- 
-/*Hence we can multiply the integers one by one, 
-only preserving the last digit (take it modulo 10 whenever possible), and stop when it becomes 0. 
-It's obvious that at most 10 multiplications are needed before stopping, 
-and it's not hard to prove a tighter upper bound of 5.*/
